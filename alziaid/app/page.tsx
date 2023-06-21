3c5c121f3d0c7cb19900b31f435b062319000d65
Kevin Sun <kevinsun@Kevins-MacBook-Air.local>
2023-06-21 22:21:54 +0800

    started over with js

--- alziaid/app/page.tsx
+++ alziaid/app/page.tsx
@@ -29,6 +29,24 @@ export default function Home() {
       </div>
 
      
+      <div className="mb-32 grid text-center justify-center lg:mb-0 ">
+        
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-5xl font-semibold`}>
+            START TEST{' '}
+            
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Diagnose Alzheimer's Disease in less than 5 minutes
+          </p>
+        </a>
+      </div>
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         
--- alziaid/app/test/page.tsx
+++ alziaid/app/test/page.tsx
@@ -1,7 +1,43 @@
 export default async function Testpage(){
     return (
-        <div>
-            <h1>TESTING</h1>
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-center p-12">
+        <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+            <div class="box-border h-16 w-32 p-4 border-4">
+                Go Back 
+            </div>
+            <div class="px-8"></div>
+            <div class="box-border h-32 w-64 p-4 border-4">
+                Time remaining: 
+            </div>
+            <div class="px-8"></div>
+            <div class="box-border h-32 w-64 p-4 border-4">
+                Responsiveness: 
+            </div>
+            <div class="px-8"></div>
+            <div class="box-border h-32 w-64 p-4 border-4">
+                 AD index: 
+            </div>
+            <div class="px-8"></div>
+
+      </div>
+      <div class="py-1"></div>
+      
+      <div class=" shadow-2xl box-border grow min-w-full min-h-full p-4 border-4">      
+                placeholder camera     
+      </div>
+      <div class="py-1"></div>
+      <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-1 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            End test{' '}
+          </h2>
+        </a>
+
+      
+    </main>
     );
 }
