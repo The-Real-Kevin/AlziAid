3e8cc71180f82a859d32369f6d76d781b40dd8b2
muffinized <66737626+muffinized@users.noreply.github.com>
2024-07-14 17:56:55 +0800

    compacting each class and merging everything into js

--- site/main.js
+++ site/main.js
@@ -2,19 +2,95 @@
 hehe = 1;
 curind = 0;
 
+titles = ['Saccadic Movement', 'Anti-Saccadic Movement', 'Smooth Movement', 'Color Variation', 'Random Dot Kinematogram']
+
+descriptions = [
+    ```
+            A grey dot will be at center of screen, focus your eyes on the grey dot. When a 
+            green dot appears, look at the green dot directly as fast as you can, until it disappears again. 
+            </br>
+            </br>螢幕中央將出現一個灰點，將眼睛聚焦在灰點上。 當綠點出現時，盡可能快速地直視綠點，直到它再次消失。
+            </br>.
+    ```,
+    ```
+            When one of the green dots flashes, look at the OTHER dot that did not flash.
+            </br>
+            </br>螢幕中央有一個灰點，兩側各有兩個綠點。 當其中一個綠點閃爍時，請查看另一個未閃爍的點。
+            </br>
+            </br>.
+    ```,
+    ```
+    
+            A green dot will smoothly move around the page. Please use your eyes to follow the green dot’s movement.
+            <br>
+            <br>一個綠點將平滑地在頁面上移動。 請用眼睛跟隨綠點的移動。
+            <br>
+            </br>.
+    ```,
+    ```
+            One dot will show on the screen, with four more surrounding it. Please click 
+            on the dot that is closest in color to the center dot. Do NOT click the center dot. 
+            </br>
+            </br>螢幕上將顯示一個點，周圍還有四個點。 請點選顏色最接近中心點的點。 不要點擊中心點。
+            </br> .
+    ```,
+    ```
+            A circle will appear on screen with small dots moving within. Four choices will appear. Please 
+            pick the choice that best describes the movement direction of the dots inside the circle. 
+            </br>
+            </br>螢幕上將出現一個圓圈，其中有小點在移動。 將出現四個選擇。 請選擇最能描述圓圈內點的移動方向的選項。
+
+    ```
+]
+
 function bclick() {
     document.getElementsByClassName("tempdisplay")[0].style.display="none";
     startTests();
     if (hehe != 1) {
         resumeRecording();
+    } else {
+        startRecording();
     }
 }
 
+function createTest() {
+    const container = document.createElement('div');
+    container.className = 'tempdisplay';
+    
+    const heading = document.createElement('h1');
+    heading.textContent = `Test ${hehe}/5: ${titles[curind]}`;
+    container.appendChild(heading);
+    
+    const instructionPara = document.createElement('p');
+    instructionPara.className = 'error';
+    instructionPara.innerHTML = descriptions[curind];
+    container.appendChild(instructionPara);
+    
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.className = 'buttons';
+    
+    const placeholderButton = document.createElement('button');
+    placeholderButton.className = 'invis';
+    placeholderButton.textContent = 'This is placeholder';
+    buttonsDiv.appendChild(placeholderButton);
+    
+    const continueButton = document.createElement('button');
+    continueButton.className = 'yea';
+    continueButton.textContent = 'Continue';
+    continueButton.onclick = function() {
+        bclick();
+        startTest();
+    };
+    buttonsDiv.appendChild(continueButton);
+    container.appendChild(buttonsDiv);
+    document.body.appendChild(container);
+}
 function why() {
     $('.test'+ hehe).remove();
     hehe += 1;
     curind += 1;
     $("#includedContent").load("test_" + hehe + ".html");
+    createTest();
 }
 
 
